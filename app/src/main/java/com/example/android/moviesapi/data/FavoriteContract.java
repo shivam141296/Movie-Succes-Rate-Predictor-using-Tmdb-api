32ad42478a75c0cdc02b5fc05dce63ee0cca4f20
shivam141296 <kapoorshivam1.sk@gmail.com>
2018-05-08 07:42:50 +0530

    Favourite Movies(java files)

---
+++ app/src/main/java/com/example/android/moviesapi/data/FavoriteContract.java
@@ -0,0 +1,18 @@
+package com.example.android.moviesapi.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by user on 08-05-2018.
+ */
+
+public class FavoriteContract {
+    public static final class FavoriteEntry implements BaseColumns {
+        public static final String TABLE_NAME = "favorite";
+        public static final String COLUMN_MOVIEID = "movieid";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_USERRATING = "userrating";
+        public static final String COLUMN_POSTER_PATH = "posterpath";
+        public static final String COLUMN_PLOT_SYNOPSIS = "overview";
+    }
+}
